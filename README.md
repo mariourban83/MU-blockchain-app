f34b5e6114deb807b305380e7112bfe62eec56b8
mariourban83 <mario.urban.83@gmail.com>
2021-01-18 12:49:57 +0000

    Create Mine_block function and first block in the chain

---
+++ README.md
@@ -0,0 +1,1 @@
+## Python - React - Blockchain Application
--- block.py
+++ block.py
@@ -1,18 +1,47 @@
+import time
+
+def mine_block(last_block, data):
+    '''
+    Mines a block based on the last block and data
+    '''
+
+    timestamp = time.time()
+    last_hash = last_block.hash
+    hash = f'{timestamp}-{last_hash}'
+
+    return Block(timestamp, last_hash, hash, data)
+
+def genesis():
+    '''
+    Genereate very first block of the chain
+    '''
+    return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
+
 class Block:
     '''
     Block: a unit of storage that stores transactions in a blockchain.
     '''
-    def __init__(self, data):
+    def __init__(self, timestamp, last_hash, hash, data,):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
         self.data = data
 
     def __repr__(self):
-        return f'Block-data: {self.data}'
+        return (
+            'Block('
+            f'timestamp: {self.timestamp},'
+            f'last_hash: {self.last_hash},'
+            f'hash: {self.hash},'
+            f'data: {self.data})'
+        )
 
 # testing only
 def main():
-    block = Block('testBlock')
+    genesis_block = genesis()
+    block = mine_block(genesis_block, 'testString')
     print(block)
-    print(f'block.py __name__: {__name__}')
 
 if __name__ == '__main__':
     main()
