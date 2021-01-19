7df0f6aaa304335a4c4d60d04c00c9394d124048
mariourban83 <mario.urban.83@gmail.com>
2021-01-19 19:30:20 +0000

    Create function for adjusting difficulty value dynamically based on the mine_rate

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,5 +1,7 @@
 import time
+
 from backend.util.crypto_hash import crypto_hash
+from backend.config import MINE_RATE
 
 GENESIS_DATA = {
     'timestamp': 1,
@@ -42,12 +44,14 @@ class Block:
 
         timestamp = time.time()
         last_hash = last_block.hash
-        difficulty = last_block.difficulty
+        difficulty = Block.adjust_difficulty(last_block, timestamp)
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         while hash[0:difficulty] != '0'* difficulty:
             nonce +=1
+            timestamp = time.time()
+            difficulty = Block.adjust_difficulty(last_block, timestamp)
             hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         return Block(timestamp, last_hash, hash, data, difficulty, nonce)
@@ -58,6 +62,20 @@ class Block:
         Genereate very first block of the chain
         '''
         return Block(**GENESIS_DATA)
+
+    @staticmethod
+    def adjust_difficulty(last_block, new_timestamp):
+        '''
+        Calculate and adjust(increase or decrease) difficulty for new mined blocks.
+        Based on MINE_RATE
+        '''
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
+
+        if (last_block.difficulty -1) > 0:
+            return last_block.difficulty -1
+        
+        return 1
 
 
 # testing only
