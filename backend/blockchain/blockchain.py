4ece162875b771c0e9747bb07751ba6e1b46aaff
mariourban83 <mario.urban.83@gmail.com>
2021-01-22 19:55:23 +0000

    Add method for validating the blockchain, tests included

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -16,15 +16,28 @@ class Blockchain:
     def __repr__(self):
         return f'Blockchain: {self.chain}'
 
+    @staticmethod
+    def is_valid_chain(chain):
+        '''
+        Validate the incoming chain by enforcing:
+        - Chain must beggin with genesis block
+        - Every block must be formatted correctly
+        '''
+
+        if chain[0] != Block.genesis():
+            raise Exception('The genesis block must be valid')
+
+        for i in range(1, len(chain)):
+            block = chain[i]
+            last_block = chain[i-1]
+            Block.is_valid_block(last_block, block)
+
+
 #  testing only
 def main():
     blockchain = Blockchain()
     blockchain.add_block('one')
     blockchain.add_block('two')
-    blockchain.add_block('three')
-    blockchain.add_block('four')
-    # blockchain.add_block('five')
-    # blockchain.add_block('six')
 
     print(blockchain)
 
