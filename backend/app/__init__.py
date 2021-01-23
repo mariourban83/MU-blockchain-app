b22f86b001a7b4096906d1c185fdd90eb5200b65
mariourban83 <mario.urban.83@gmail.com>
2021-01-23 23:23:26 +0000

    Add API endpoint for mining blocks with Flask - /blockchain/mine route

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,9 +5,6 @@ from backend.blockchain.blockchain import Blockchain
 app = Flask(__name__)
 blockchain = Blockchain()
 
-for i in range(5):
-    blockchain.add_block(i)
-
 
 @app.route("/")
 def route_default():
@@ -16,5 +13,12 @@ def route_default():
 @app.route('/blockchain')
 def route_blockchain():
     return jsonify(blockchain.to_json())
+
+@app.route('/blockchain/mine')
+def route_blockchain_mine():
+    transaction_data = "test transaction data"
+    blockchain.add_block(transaction_data)
+
+    return jsonify(blockchain.chain[-1].to_json())
 
 app.run()
