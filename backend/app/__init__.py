c4877221032bd321767a90b734fb22fb6e88f5c8
mariourban83 <mario.urban.83@gmail.com>
2021-02-02 05:46:40 +0000

    Add if statement for checking for existing transaction in the pool

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -35,10 +35,19 @@ def route_blockchain_mine():
 @app.route('/wallet/transact', methods=['POST'])
 def route_wallet_transact():
     transaction_data = request.get_json()
-    transaction = Transaction(
-        wallet,
-        transaction_data['recipient'],
-        transaction_data['amount']
+    transaction = transaction_pool.existing_transaction(wallet.address)
+    
+    if transaction:
+        transaction.update(
+            wallet,
+            transaction_data['recipient'],
+            transaction_data['amount']
+        )
+    else:
+        transaction = Transaction(
+            wallet,
+            transaction_data['recipient'],
+            transaction_data['amount']
         )
 
     pubsub.broadcast_transaction(transaction)
