edc9fcabf2b32f7d5710106d695ae823babbc7c2
mariourban83 <mario.urban.83@gmail.com>
2021-02-02 02:35:18 +0000

    Add methods for encoding signature

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -4,6 +4,10 @@ import json
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric.utils import (
+    encode_dss_signature,
+    decode_dss_signature
+)
 from cryptography.exceptions import InvalidSignature
 
 from backend.config import STARTING_BALANCE
@@ -28,10 +32,10 @@ class Wallet:
         '''
         Generate a signature based on the data using the local private key.
         '''
-        return self.private_key.sign(
+        return decode_dss_signature(self.private_key.sign(
             json.dumps(data).encode('utf-8'), 
             ec.ECDSA(hashes.SHA256())
-            )
+            ))
 
     def serialize_public_key(self):
         '''
@@ -51,9 +55,12 @@ class Wallet:
             public_key.encode('utf-8'),
             default_backend()
         )
+
+        (r, s) = signature
+
         try:
             deserialized_public_key.verify(
-                signature,
+                encode_dss_signature(r,s),
                 json.dumps(data).encode('utf_8'),
                 ec.ECDSA(hashes.SHA256())
                 )
