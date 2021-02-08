f05054d41b810bf3824c0655b0ccb441bf4def1a
mariourban83 <mario.urban.83@gmail.com>
2021-02-08 19:29:07 +0000

    Add transaction chain validations method

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -1,5 +1,7 @@
 
 from backend.blockchain.block import Block
+from backend.wallet.transaction import Transaction
+from backend.config import MINING_REWARD_INPUT
 
 class Blockchain:
     '''
@@ -65,6 +67,34 @@ class Blockchain:
             last_block = chain[i-1]
             Block.is_valid_block(last_block, block)
 
+    @staticmethod
+    def is_valid_transaction_chain(chain):
+        '''
+        Enforce the rules of chain composed of blocks of transactions
+            Each transaction must only appear once in the chain
+            There can only be one mining reward per mined block
+            Each transaction must be valid
+            '''
+        transaction_ids = set()
+
+        for block in chain():
+            has_mining_reward = False
+
+            for transaction_json in block.data:
+                transaction = Transaction.from_json(transaction_json)
+
+                if transaction.input == MINING_REWARD_INPUT:
+                    if has_mining_reward:
+                        raise Exception('There can only be one mining reward per block' \
+                            f'Check block with hash : {block.hash}')
+                    has_mining_reward = True
+
+                if transaction.id in transaction_ids:
+                    raise Exception(f'Transaction {transaction.id} is not unique')
+
+                transaction_ids.add(transaction.id)
+
+                Transaction.is_valid_transaction(transaction)
 
 #  testing only
 def main():
