c9596d8f1d37e1572b2e7f61c64ed67b060556a6
mariourban83 <mario.urban.83@gmail.com>
2021-02-21 00:22:12 +0000

    Add transaction component with toggle data functionality

--- frontend/src/components/Block.js
+++ frontend/src/components/Block.js
@@ -1,8 +1,51 @@
-import React from 'react'
-import { MILLISECONDS_PY } from '../config'
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { MILLISECONDS_PY } from '../config';
+import Transaction from './Transaction';
+
+function ToggleTransactionDisplay({ block }) {
+    const [displayTransaction, setDisplayTransaction] = useState(false);
+    const {data} = block;
+
+    const toggleDisplayTransaction = () => {
+        setDisplayTransaction(!displayTransaction)
+    }
+
+    if (displayTransaction) {
+        return (
+            <div>{data.map(transaction => (
+                <div key={transaction.id}>
+                    <hr />
+                    <Transaction transaction={transaction} />
+                </div>
+            ))}
+                <br />
+                <Button 
+                    variant='warning'
+                    size='sm'
+                    onClick={toggleDisplayTransaction}
+                    >
+                    Show Less
+                </Button>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <br />
+            <Button 
+                variant='info'
+                size='sm'
+                onClick={toggleDisplayTransaction}
+                >
+                Show More
+            </Button>
+        </div>
+    )
+}
 
 function Block({ block }) {
-    const {timestamp, hash, data} = block
+    const {timestamp, hash} = block
     const hashDisplay = `${hash.substring(0, 15)}...`;
     const timestampDisplay = new Date(timestamp/MILLISECONDS_PY).toLocaleString();
 
@@ -10,7 +53,7 @@ function Block({ block }) {
         <div className="Block">
             <div>Hash: {hashDisplay}</div>
             <div>Timestamp: {timestampDisplay}</div>
-            <div>{JSON.stringify(data)}</div>
+            <ToggleTransactionDisplay block={block} />
         </div>
     )
 }
