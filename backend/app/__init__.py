850b4ccd0243af5d9f1dccf1bc0b155ef30faa3e
mariourban83 <mario.urban.83@gmail.com>
2021-02-21 01:42:13 +0000

    Add option to seed the peer with data at startup

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -80,6 +80,11 @@ if os.environ.get('PEER') == 'True':
     except Exception as e:
         print(f'\n ---!! -- Error while synchronizing : {e} -- !!---')
 
-
+if os.environ.get('SEED_DATA') == 'True':
+    for i in range(10):
+        blockchain.add_block([
+            Transaction(Wallet(), Wallet().address, random.randint(2, 50)).to_json(),
+            Transaction(Wallet(), Wallet().address, random.randint(2, 50)).to_json()
+        ])
 
 app.run(port=PORT)
